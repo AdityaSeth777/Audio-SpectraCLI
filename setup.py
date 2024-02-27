ba0c5538ec20f7bd4e3934e1da6243b032923fe7
AdityaSeth777 <contact@adityaseth.in>
2024-02-27 20:37:11 +0530

    Little fixes before publishing

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='Audio_SpectraCLI',
     version='0.1',
+    description="""AudioSpectraCLI is a command-line tool that provides real-time FFT visualization of audio spectra. It captures audio input from the microphone and displays the corresponding frequency spectrum directly in the terminal window, allowing users to monitor and analyze audio signals without the need for graphical interfaces.""",
+    author="Aditya Seth",
     packages=find_packages(),
     install_requires=[
         'numpy',
