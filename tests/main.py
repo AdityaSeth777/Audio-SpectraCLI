a126987b9afe22f8f363da094f159d4140546460
AdityaSeth777 <contact@adityaseth.in>
2024-03-30 18:19:18 +0530

    Bored. I'll start after my exams day after tomorrow.

--- tests/main.py
+++ tests/main.py
@@ -2,7 +2,7 @@ from Audio_SpectraCLI import AudioSpectrumVisualizer
 
 # Create an instance of AudioSpectrumVisualizer with custom parameters
 audio_visualizer = AudioSpectrumVisualizer(
-    duration=5, frequency_range=(50, 5000), color='red')
+    duration=5, frequency_range=(50, 5000), color='green')
 
 # Start the audio spectrum visualization
 audio_visualizer.start_visualization()
