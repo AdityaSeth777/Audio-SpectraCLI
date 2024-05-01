8e082a84fef0394cd5a1f3495b90f7cd52f408d1
AdityaSeth777 <contact@adityaseth.in>
2024-05-01 11:50:42 +0530

    Hit a few bumps. Will do

--- Audio_SpectraCLI/main.py
+++ Audio_SpectraCLI/main.py
@@ -1,18 +1,16 @@
+# Aditya Seth
+# Description: This file contains the main code for the Audio-SpectraCLI project. It is responsible for creating the AudioSpectrumVisualizer class which is used to visualize the audio spectrum in real-time.
+
 import numpy as np
 import matplotlib.pyplot as plt
 import sounddevice as sd
 import queue
 import threading
 import time
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget
-from PyQt5.QtCore import QTimer, QObject, pyqtSignal, pyqtSlot
 
 
-class AudioWorker(QObject):
-    finished = pyqtSignal()
-
-    def __init__(self, duration, fs, block_size, frequency_range, color):
-        super().__init__()
+class AudioSpectrumVisualizer:
+    def __init__(self, duration=10, fs=44100, block_size=2048, frequency_range=(20, 20000), color='blue'):
         self.duration = duration
         self.fs = fs
         self.block_size = block_size
@@ -21,61 +19,44 @@ class AudioWorker(QObject):
         self.color = color
         self.audio_queue = queue.Queue()
 
-    @pyqtSlot()
+    def audio_callback(self, indata, frames, time, status):
+        if status:
+            print(status)
+        self.audio_queue.put(indata.copy())
+
     def process_audio(self):
-        freq_bins = np.fft.fftfreq(self.block_size, 1 / self.fs)
+        freq_bins = np.fft.fftfreq(self.block_size, 1/self.fs)
         spectrum = np.zeros(self.block_size)
 
         plt.ion()
-        self.fig, self.ax = plt.subplots()
-        self.x = np.arange(0, self.block_size)
-        self.line, = self.ax.plot(freq_bins, spectrum, color=self.color)
-        self.ax.set_xlim(self.frequency_range)
-        self.ax.set_ylim(0, 0.1)
-        self.ax.set_title('Audio-SpectraCLI Visualization')
-        self.ax.set_xlabel('Frequency (Hz)')
-        self.ax.set_ylabel('Magnitude')
+        fig, ax = plt.subplots()
+        x = np.arange(0, self.block_size)
+        line, = ax.plot(freq_bins, spectrum, color=self.color)
+        ax.set_xlim(self.frequency_range)
+        ax.set_ylim(0, 0.1)
+        ax.set_xlabel('Frequency (Hz)')
+        ax.set_ylabel('Magnitude')
 
         while True:
             audio_block = self.audio_queue.get()
             spectrum = np.abs(np.fft.fft(audio_block[:, 0], n=self.block_size))
             max_magnitude = np.max(spectrum)
-            if max_magnitude > self.ax.get_ylim()[1]:
-                self.ax.set_ylim(0, max_magnitude * 1.1)
-            self.line.set_ydata(spectrum)
-            self.fig.canvas.draw()
-            self.fig.canvas.flush_events()
-
-    def start(self):
-        self.thread = threading.Thread(target=self.process_audio, daemon=True)
-        self.thread.start()
-
-
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle('Audio-SpectraCLI')
-        self.setGeometry(100, 100, 600, 400)
-
-        self.start_button = QPushButton('Start Visualization')
-        self.start_button.clicked.connect(self.start_visualization)
-
-        layout = QVBoxLayout()
-        layout.addWidget(self.start_button)
-
-        central_widget = QWidget()
-        central_widget.setLayout(layout)
-        self.setCentralWidget(central_widget)
-
-        self.audio_worker = AudioWorker(
-            duration=10, fs=44100, block_size=2048, frequency_range=(20, 20000), color='blue')
+            if max_magnitude > ax.get_ylim()[1]:
+                ax.set_ylim(0, max_magnitude * 1.1)
+            line.set_ydata(spectrum)
+            fig.canvas.draw()
+            fig.canvas.flush_events()
 
     def start_visualization(self):
-        self.audio_worker.start()
+        audio_thread = threading.Thread(target=self.process_audio, daemon=True)
+        audio_thread.start()
+
+        with sd.InputStream(callback=self.audio_callback, channels=1, samplerate=self.fs):
+            while True:
+                time.sleep(1)
+
+        audio_thread.join()
 
 
-if __name__ == '__main__':
-    app = QApplication([])
-    window = MainWindow()
-    window.show()
-    app.exec_()
+# List of functions available for use:
+__all__ = ['AudioSpectrumVisualizer']
