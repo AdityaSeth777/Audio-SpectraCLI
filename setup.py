f6df0bb1a569ea7f17d439ea99521eade06b4f69
AdityaSeth777 <contact@adityaseth.in>
2024-05-23 17:56:16 +0530

    v4.0.0 - Launched.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Audio_SpectraCLI',
-    version='4.0',
+    version='4.0.0',
     author="Aditya Seth",
     long_description=open('Readme.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
