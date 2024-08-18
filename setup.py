b402db4bbb312993974d9c1fd0815269963f6356
AdityaSeth777 <contact@adityaseth.in>
2024-08-18 18:33:54 +0530

    need to finish this - conda

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
+import os
 from setuptools import setup, find_packages
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+try:
+    with open(os.path.join(this_directory, 'Readme.md'), encoding='utf-8') as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = 'Long description not found.'
 
 setup(
     name='Audio_SpectraCLI',
     version='4.0.1',
     author="Aditya Seth",
-    long_description=open('Readme.md', encoding='utf-8').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     packages=find_packages(),
     install_requires=[
